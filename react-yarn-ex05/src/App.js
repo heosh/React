00b108c583a407978a56f783c00bfb21c45a4203
heosh <81971258+heosh@users.noreply.github.com>
2021-10-06 23:46:29 +0900

    Iteration, map 사용

---
+++ react-yarn-ex05/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import IterationSample from './Components/IterationSample';
+
+
+
+function App() {
+  return (
+    <IterationSample>
+    </IterationSample>
+  );
+}
+
+export default App;
