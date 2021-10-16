b7b9586dc53e9e460edf3894297d51f6212d5718
heosh <81971258+heosh@users.noreply.github.com>
2021-10-16 17:31:46 +0900

    Add files via upload

---
+++ react-ex03-useState/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Counter from './Components/Counter';
+import Say from './Components/Say';
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <Say />
+    </div>
+  );
+}
+
+export default App;
